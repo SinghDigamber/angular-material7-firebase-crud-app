4e84b0fcd9daaff9f940dfdcc70662164e0e7139
SinghDigamber <digamber@positronx.io>
2019-05-28 15:36:21 +0530

    security update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCcm4l371tvw-11rjo_WGnovBEbpJGU4RI",
-    authDomain: "my-crud-app-78b3d.firebaseapp.com",
-    databaseURL: "https://my-crud-app-78b3d.firebaseio.com",
-    projectId: "my-crud-app-78b3d",
-    storageBucket: "my-crud-app-78b3d.appspot.com",
-    messagingSenderId: "1060052209979",
-    appId: "1:1060052209979:web:bbff1d3d6ebba133"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 };
